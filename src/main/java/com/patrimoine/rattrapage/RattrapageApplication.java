c23130c143ae99f9acb41490c3d9635f7f68defb
Micael Jerry <hei.jerry.2@gmail.com>
2024-09-23 13:38:54 +0300

    init

---
+++ src/main/java/com/patrimoine/rattrapage/RattrapageApplication.java
@@ -0,0 +1,13 @@
+package com.patrimoine.rattrapage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RattrapageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RattrapageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrimoine/rattrapage/RattrapageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrimoine.rattrapage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RattrapageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
